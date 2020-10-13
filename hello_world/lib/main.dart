593e7d12f5025d4b19c420fe6034de73fd2a6cfb
franckalainai <franckalain.ai@gmail.com>
2020-10-13 14:50:37 +0000

    first commit

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: Container(),
+    ),
+  ));
+}
